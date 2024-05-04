b6f7a2135832d150a280589c865f81b437ab21ce
shuvo-99 <anontoshuvo99@gmail.com>
2024-05-04 20:51:33 +0600

    JavaScript

---
+++ 158A - Next Round.js
@@ -0,0 +1,12 @@
+var inpArr = readline().split(" ");
+function nextRound(place) {
+  var count = 0;
+  var inp = readline().split(" ");
+  for (i = 0; i < inp.length; i++) {
+    if (parseInt(inp[i]) > 0 && parseInt(inp[i]) >= parseInt(inp[place - 1])) {
+      count += 1;
+    }
+  }
+  print(count);
+}
+nextRound(parseInt(inpArr[1]));
